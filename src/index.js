deae06a3754978f0d3458dbb32c5f94e81e21bd0
sri697 <sri694040@gmail.com>
2023-10-24 18:33:47 +0530

    main DND

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Input from './Input';
 import Todolist from './Todolist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
